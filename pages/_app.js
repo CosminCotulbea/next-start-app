ef958881c92e402f432ca2ca7c77ac364b71f023
Cosmin Cotulbea <cosmin.cotulbea@roweb.ro>
2021-10-27 21:40:50 +0300

    Readme

--- pages/_app.js
+++ pages/_app.js
@@ -10,15 +10,11 @@ const MyApp = ({Component, pageProps}) => {
     const getLayout = Component.getLayout || ((page) => page);
     return <>
         <Head>
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <meta charSet="UTF-8" />
-            <meta charSet="UTF-8" />
-                    <link rel="shortcut icon" href="/fav.ico" />
-                    <link rel="stylesheet"
-                          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
-                          integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
-                          crossOrigin="anonymous" />
-            <link rel="shortcut icon" href="/fav.ico" />
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <meta charSet="UTF-8"/>
+            <meta charSet="UTF-8"/>
+            <link rel="shortcut icon" href="/fav.ico"/>
+            <title>Next start app</title>
         </Head>
         <Provider store={store}>
             {getLayout(<Component {...pageProps} />)}
